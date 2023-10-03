4eba206a20871198305fe32db17a33f8d4f1a939
Hossein-Asadi <hosseinasd0109@gmail.com>
2023-10-03 11:17:23 +0330

    create PhoneCard component

--- Desktop/landing page/vite-project/src/components/Main/Main.tsx
+++ Desktop/landing page/vite-project/src/components/Main/Main.tsx
@@ -4,11 +4,40 @@ import done from "../../assets/images/done.png";
 import Card from "./Card";
 import CountCard from "./CountCard";
 import FeedBackSection from "./FeedBackSection";
+import "./index.css";
+import PhoneCard from "./PhoneCard";
 
 export default function Main() {
   return (
-    <div className="w-full flex flex-col justify-center items-center px-20">
-      <FeedBackSection />
+    <div className="w-full flex flex-col justify-center items-center">
+      <div className="w-full px-10 flex flex-row justify-between gap-10">
+        <div className="flex flex-row justify-center items-center py-4 gap-20">
+          <PhoneCard
+            header="Welcome"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+            imgSrc={welcome}
+            imgAlt="Welcome"
+          />
+          <PhoneCard
+            header="Welcome"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+            imgSrc={welcome}
+            imgAlt="Welcome"
+          />
+          <PhoneCard
+            header="Welcome"
+            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+            imgSrc={welcome}
+            imgAlt="Welcome"
+          />
+        </div>
+      </div>
+
+      {/* <div className="w-full">
+        <div className="trapezoid"></div>
+      </div> */}
+
+      {/* <FeedBackSection /> */}
       {/* <div className="w-full flex flex-row flex-wrap justify-center items-center px-20 py-3 gap-20">
         <CountCard imgSrc={done} text="Projects Complete" count="100+" />
         <CountCard imgSrc={done} text="Projects Complete" count="100+" />
--- Desktop/landing page/vite-project/src/components/Main/PhoneCard/PhoneCard.tsx
+++ Desktop/landing page/vite-project/src/components/Main/PhoneCard/PhoneCard.tsx
@@ -1,10 +1,42 @@
+import "./index.css";
 interface PhoneCardProps {
+  header: string;
+  text: string;
+  imgSrc: string;
+  imgAlt: string;
 }
 
-export default function PhoneCard({}: PhoneCardProps) {
-    return(
-        <div>
-            
-        </div>
-    )
+export default function PhoneCard({
+  header,
+  text,
+  imgSrc,
+  imgAlt,
+}: PhoneCardProps) {
+  return (
+    <div className="phone flex flex-col justify-center items-center w-[240px] bg-white border-4 border-gray-200 rounded-2xl shadow pt-14 px-1 pb-10 ">
+      <div className="notch-container">
+        <div className="notch"></div>
+      </div>
+      <img
+        className="h-[160px] w-[240px] px-3 pb-4 rounded-lg"
+        src={imgSrc}
+        alt={imgAlt}
+      />
+      <div className="px-1 pb-3">
+        <a href="#">
+          <p className="text-lg text-center font-semibold tracking-tight text-gray-900 dark:text-white">
+            {header}
+          </p>
+        </a>
+        <p className="text-center text-sm text-gray-500">{text}</p>
+      </div>
+      <div className="flex flex-row gap-28 pt-4">
+        <a href="#">Skip</a>
+        <a href="#">Next</a>
+      </div>
+      <div className="end-container">
+        <div className="notch"></div>
+      </div>
+    </div>
+  );
 }
